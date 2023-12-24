78321733b87040f75654cf4326c9c76f9129b357
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-12-24 19:49:36 +0100

    hero content

--- apps/nextjs/app/page.tsx
+++ apps/nextjs/app/page.tsx
@@ -1,7 +1,14 @@
 export default function Home() {
   return (
     <main>
-       hell o
+      <div className="text-center">
+        <h1 className="text-6xl font-serif">
+          Heading
+        </h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt, tempora?
+        </p>
+      </div>
     </main>
   )
 }
