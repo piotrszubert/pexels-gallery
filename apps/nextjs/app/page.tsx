a4b29859e5a690492ea773c1ca132ac6ecdf0c7b
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-12-26 19:55:45 +0100

    hero as component

--- apps/nextjs/app/page.tsx
+++ apps/nextjs/app/page.tsx
@@ -1,17 +1,9 @@
+import { Hero } from "@/components/hero";
+
 export default function Home() {
   return (
     <main>
-      <div className="text-center space-y-4">
-        <h1 className="text-6xl font-serif italic">
-          Heading
-        </h1>
-        <p className="max-w-sm mx-auto">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt, tempora?
-        </p>
-        <button className="px-3 py-2 text-sm rounded-xl bg-purple-500">
-          Get started
-        </button>
-      </div>
+      <Hero />
     </main>
   )
 }
---
+++ apps/nextjs/components/hero.tsx
@@ -0,0 +1,15 @@
+export const Hero = () => {
+  return (
+    <div className="text-center space-y-4">
+      <h1 className="text-6xl font-serif italic">
+        Heading
+      </h1>
+      <p className="max-w-sm mx-auto">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt, tempora?
+      </p>
+      <button className="px-3 py-2 text-sm rounded-xl bg-purple-500">
+        Get started
+      </button>
+    </div>
+  );
+}
