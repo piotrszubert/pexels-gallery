c45cf2f11ccfeb4ba5522e327297bf50829de025
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-12-27 12:29:41 +0100

    gallery layout prototype

--- apps/nextjs/app/page.tsx
+++ apps/nextjs/app/page.tsx
@@ -1,9 +1,32 @@
 import { Hero } from "@/components/hero";
 
+const images = [
+  {url: "https://via.placeholder.com/200x400", title: ""},
+  {url: "https://via.placeholder.com/200x400", title: ""},
+  {url: "https://via.placeholder.com/200x400", title: ""},
+  {url: "https://via.placeholder.com/200x400", title: ""},
+]
+
 export default function Home() {
   return (
     <main>
       <Hero />
+      <div className="grid gap-6 grid-cols-4 max-w-4xl mx-auto mt-24">
+        {images.map((image, index) => (
+          <div
+            key={index}
+            className="bg-slate-300 min-h-[300px] min-w-[100px] rounded-xl"
+          > 
+          </div>
+        ))} 
+        {images.map((image, index) => (
+          <div
+            key={index}
+            className="bg-slate-300 min-h-[300px] min-w-[100px] rounded-xl"
+          > 
+          </div>
+        ))}        
+      </div>
     </main>
   )
 }
