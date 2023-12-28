c77bfd3066b0a057572b39bd9b9c9e1af7f4f5a4
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-12-28 10:15:58 +0100

    responsive grid

--- apps/nextjs/app/page.tsx
+++ apps/nextjs/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <main>
       <Hero />
-      <div className="grid gap-6 grid-cols-4 max-w-4xl mx-auto mt-24">
+      <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 max-w-4xl mx-auto mt-24">
         {images.map((image, index) => (
           <div
             key={index}
@@ -19,13 +19,6 @@ export default function Home() {
           > 
           </div>
         ))} 
-        {images.map((image, index) => (
-          <div
-            key={index}
-            className="bg-slate-300 min-h-[300px] min-w-[100px] rounded-xl"
-          > 
-          </div>
-        ))}        
       </div>
     </main>
   )
