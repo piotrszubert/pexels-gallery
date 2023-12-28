42a2830e1ccbfaa191fb869ef08fcf4fc18f2035
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-12-28 10:16:58 +0100

    added body padding

--- apps/nextjs/app/page.tsx
+++ apps/nextjs/app/page.tsx
@@ -9,7 +9,7 @@ const images = [
 
 export default function Home() {
   return (
-    <main>
+    <main className="px-4">
       <Hero />
       <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 max-w-4xl mx-auto mt-24">
         {images.map((image, index) => (
