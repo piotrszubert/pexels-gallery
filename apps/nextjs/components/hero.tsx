b911f64f187267ef80b63b3ccff89dd2b6cafae1
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-12-29 21:22:18 +0100

    updated hero colors

--- apps/nextjs/components/hero.tsx
+++ apps/nextjs/components/hero.tsx
@@ -1,10 +1,10 @@
 export const Hero = () => {
   return (
     <div className="text-center space-y-4 mt-32">
-      <h1 className="text-6xl font-serif italic">
+      <h1 className="text-6xl text-purple-200 font-serif italic">
         Heading
       </h1>
-      <p className="max-w-sm mx-auto">
+      <p className="max-w-sm mx-auto text-gray-400">
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt, tempora?
       </p>
       <button className="px-3 py-2 text-sm rounded-xl bg-purple-500 hover:bg-purple-500/90 transition-colors duration-500">
