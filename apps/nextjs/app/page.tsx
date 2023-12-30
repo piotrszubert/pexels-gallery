0f55c7e0c8b442ae23719de94d1fca4b380630d6
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-12-30 09:11:19 +0100

    moved gallery to component

--- apps/nextjs/app/page.tsx
+++ apps/nextjs/app/page.tsx
@@ -1,25 +1,11 @@
+import { Gallery } from "@/components/gallery";
 import { Hero } from "@/components/hero";
-
-const images = [
-  {url: "https://via.placeholder.com/200x400", title: ""},
-  {url: "https://via.placeholder.com/200x400", title: ""},
-  {url: "https://via.placeholder.com/200x400", title: ""},
-  {url: "https://via.placeholder.com/200x400", title: ""},
-]
 
 export default function Home() {
   return (
     <main className="px-4">
       <Hero />
-      <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 max-w-4xl mx-auto mt-24">
-        {images.map((image, index) => (
-          <div
-            key={index}
-            className="bg-slate-300 min-h-[300px] min-w-[100px] rounded-xl"
-          > 
-          </div>
-        ))} 
-      </div>
+      <Gallery />
     </main>
   )
 }
---
+++ apps/nextjs/components/gallery.tsx
@@ -0,0 +1,20 @@
+const images = [
+  { url: "https://via.placeholder.com/200x400", title: "" },
+  { url: "https://via.placeholder.com/200x400", title: "" },
+  { url: "https://via.placeholder.com/200x400", title: "" },
+  { url: "https://via.placeholder.com/200x400", title: "" },
+]
+
+export const Gallery = () => {
+  return (
+    <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 max-w-4xl mx-auto mt-24">
+      {images.map((image, index) => (
+        <div
+          key={index}
+          className="bg-slate-300 min-h-[300px] min-w-[100px] rounded-xl"
+        >
+        </div>
+      ))}
+    </div>
+  );
+}
