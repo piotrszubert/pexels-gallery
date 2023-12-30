0c4ee5d95b81b5ddf31008451cf189e7db25ca3d
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-12-30 09:16:50 +0100

    added footer

--- apps/nextjs/app/page.tsx
+++ apps/nextjs/app/page.tsx
@@ -6,6 +6,15 @@ export default function Home() {
     <main className="px-4">
       <Hero />
       <Gallery />
+      <footer className="text-gray-400 text-xs pt-8 pb-2 text-center">
+        <span className="font-serif text-xl text-purple-200 italic">
+          Heading
+        </span>
+        <br /> 
+        &copy; 2023
+        <br />
+        All rigths reserved.
+      </footer>
     </main>
   )
 }
