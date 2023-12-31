78d01dd08d024ec8f39ec31e9b55554d130586c4
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-12-31 16:18:19 +0100

    footer component

--- apps/nextjs/app/page.tsx
+++ apps/nextjs/app/page.tsx
@@ -1,3 +1,4 @@
+import { Footer } from "@/components/footer";
 import { Gallery } from "@/components/gallery";
 import { Hero } from "@/components/hero";
 
@@ -6,15 +7,7 @@ export default function Home() {
     <main className="px-4">
       <Hero />
       <Gallery />
-      <footer className="text-gray-400 text-xs pt-8 pb-2 text-center">
-        <span className="font-serif text-xl text-purple-200 italic">
-          Heading
-        </span>
-        <br /> 
-        &copy; 2023
-        <br />
-        All rigths reserved.
-      </footer>
+      <Footer />
     </main>
   )
 }
