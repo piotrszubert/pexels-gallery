36bd56bb3d75e1cce4af72d151c474a61126bdde
ps777 <56358194+piotrszubert@users.noreply.github.com>
2024-01-01 13:31:48 +0100

    setup light variant

--- apps/nextjs/components/footer.tsx
+++ apps/nextjs/components/footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
     return(
         <footer className="dark:text-gray-400 text-xs pt-8 pb-2 text-center">
-        <span className="font-serif text-xl dark:text-purple-200 italic">
+        <span className="font-serif text-xl dark:text-purple-200 text-purple-900 italic">
           Heading
         </span>
         <br /> 
--- apps/nextjs/components/hero.tsx
+++ apps/nextjs/components/hero.tsx
@@ -1,10 +1,10 @@
 export const Hero = () => {
   return (
     <div className="text-center space-y-4 mt-32">
-      <h1 className="text-6xl dark:text-purple-200 font-serif italic">
+      <h1 className="text-6xl dark:text-purple-200 text-purple-900 font-serif italic">
         Heading
       </h1>
-      <p className="max-w-sm mx-auto dark:text-gray-400">
+      <p className="max-w-sm mx-auto dark:text-gray-400 text-gray-600">
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt, tempora?
       </p>
       <button className="px-3 py-2 text-sm rounded-xl bg-purple-500 hover:bg-purple-500/90 transition-colors duration-500">
