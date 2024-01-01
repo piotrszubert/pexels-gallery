3c3098c2014731859200836042f11c48fe702cbc
ps777 <56358194+piotrszubert@users.noreply.github.com>
2024-01-01 13:29:11 +0100

    added dark modifier

--- apps/nextjs/components/footer.tsx
+++ apps/nextjs/components/footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
     return(
-        <footer className="text-gray-400 text-xs pt-8 pb-2 text-center">
-        <span className="font-serif text-xl text-purple-200 italic">
+        <footer className="dark:text-gray-400 text-xs pt-8 pb-2 text-center">
+        <span className="font-serif text-xl dark:text-purple-200 italic">
           Heading
         </span>
         <br /> 
