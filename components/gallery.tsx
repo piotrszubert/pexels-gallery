f65c0212fa83cd3c1825466d970b02b56816a038
ps777 <56358194+piotrszubert@users.noreply.github.com>
2024-01-31 11:03:12 +0000

    loading state, refactor

--- components/gallery.tsx
+++ components/gallery.tsx
@@ -1,9 +1,11 @@
 'use client';
 
 import { useState, useEffect, Fragment } from "react";
+import { ImageCard } from "./imageCard";
 
 export const Gallery = () => {
   const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchImages = async () => {
@@ -11,15 +13,15 @@ export const Gallery = () => {
         const response = await fetch(`/api/images`);
 
         if (!response.ok) {
+          setIsLoading(false);
           throw new Error("Failed to fetch data");
         }
 
         const data = await response.json();
-
+        setIsLoading(false);
         setImages(data.data.photos);
-
-        console.log(data.data.photos);
       } catch (error) {
+        setIsLoading(false);
         console.error(error);
       }
     };
@@ -27,20 +29,14 @@ export const Gallery = () => {
     fetchImages();
   }, []);
 
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 max-w-4xl mx-auto mt-24">
-      {images.map((image, index) => (
-        <Fragment
-          key={image['id']}
-        >
-          <img className="object-cover w-full h-full rounded" width={400} height={400} src={image['src']['medium']} alt="" />
-        </Fragment>
-        // <div
-        //   key={image.title}
-        //   className="border rounded-xl overflow-hidden"
-        // >
-        //   <img className="object-cover w-full h-full rounded" src={image.url + `?random=${index}`} alt={image.title} />
-        // </div>
+      {images.map((image) => (
+        <ImageCard image={image} />
       ))}
     </div>
   );
