a0573b77db6a20aa83e1267d6cc7004f9ebb2539
ps777 <56358194+piotrszubert@users.noreply.github.com>
2024-01-31 11:45:21 +0000

    removed suspense

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,12 @@
 import { Footer } from "@/components/footer"
 import { Gallery } from "@/components/gallery"
 import { Hero } from "@/components/hero"
-import { Suspense } from "react"
 
 export default function Home() {
   return (
     <main className="px-4">
       <Hero />
-      <Suspense fallback={<div className="bg-red-300">elooooo</div>}>
-        <Gallery />
-      </Suspense>
+      <Gallery />
       <Footer />
     </main>
   )
