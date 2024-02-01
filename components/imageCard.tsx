f1b6d1eaf20fcba9eb2c4b9f421895f33256de83
ps777 <56358194+piotrszubert@users.noreply.github.com>
2024-02-01 14:53:00 +0000

    card hover effect

--- components/imageCard.tsx
+++ components/imageCard.tsx
@@ -17,15 +17,29 @@ export const ImageCard = ({
   }
 
   return (
-    <div className="relative h-[500px] md:h-[400px]">
+    <div className="relative h-[500px] md:h-[400px] overflow-hidden rounded group">
       <Image
-        className="object-cover rounded"
+        className="object-cover group-hover:scle-100 transition-all duration-300"
         fill
         sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
         loading="lazy"
         src={image?.src?.large!}
         alt={image?.alt ? image?.alt : "image"}
       />
+      <div className="absolute overflow-hidden -bottom-10 group-hover:bottom-0 w-full transition-all duration-300">
+        <div className="py-2 px-3 backdrop-blur-sm bg-purple-900/60 w-full">
+          <button>
+            Like
+          </button>
+        </div>
+      </div>
+      {/* <div className="absolute overflow-hidden -top-10 group-hover:top-0 w-full transition-all duration-300">
+        <div className="py-2 px-3 backdrop-blur-sm bg-purple-900/60 w-full">
+          <button>
+            Like
+          </button>
+        </div>
+      </div> */}
     </div>
   )
 }
